27c4574778da6678cd6d2f01c15a46be231afb05
dennisjjagwe <dennisjjagwe@gmail.com>
2017-10-23 21:36:49 +0300

    Added application secret key

--- api/__init__.py
+++ api/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:mypassword@127.0.0.1:5432/yummy_recipes"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "THhdtgUhhkdldyrhfkfu8369kslo09wjjw"
 db = SQLAlchemy(app)
 
 
