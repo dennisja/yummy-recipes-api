184fe4a042e8f3513986fbb4fca36df9553f9741
dennisjjagwe <dennisjjagwe@gmail.com>
2017-10-25 21:32:25 +0300

    Added user_must_own_recipe decorator It restricts modification of a recipe by a user who doesnot own it

--- api/decorators.py
+++ api/decorators.py
@@ -1,7 +1,7 @@
 from functools import wraps
 from api.helpers import Secure
 from flask import request, jsonify
-from api.models import User
+from api.models import User, Recipe
 
 
 def json_data_required(decorated_func):
@@ -36,5 +36,35 @@ def auth_token_required(decorated_func):
 
         # call decorated function passing the current user object as the first argument
         return decorated_func(current_user, *args, **kwargs)
+
+    return wrapper
+
+
+def user_must_own_recipe(decorated_func):
+    """
+        Ensures that a recipe exists and the user must own a recipe before he changes anything about it
+        This decorator must be used below the auth_token_required decorator
+    """
+
+    @wraps(decorated_func)
+    def wrapper(*args, **kwargs):
+
+        # code to run before decorated function runs
+        user = args[0]
+        recipe_id = kwargs.get("recipe_id",None)
+
+        recipe = Recipe.query.filter_by(id=recipe_id).first()
+
+        if not recipe:
+            return jsonify({"errors": ["Trying to access a recipe that does not exist"]}), 404
+
+        if recipe.owner != user.id:
+            return jsonify({"errors": ["You are trying to modify a recipe that does not belong to you"]}), 403
+
+        # remove recipe id to avoid multiple arguments error
+        kwargs.pop("recipe_id")
+
+        # call decorated function
+        return decorated_func(user, recipe, *args, **kwargs)
 
     return wrapper
