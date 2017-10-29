96271a3252d8fdbfb10672d74f57c07fc5f0bfe4
dennisja <dennisjjagwe@gmail.com>
2017-10-29 12:01:05 +0300

    Add user_must_own_recipe_category decorator It ensures that a user edits, or deletes a recipe category which exists and only belongs to him

--- api/decorators.py
+++ api/decorators.py
@@ -1,7 +1,7 @@
 from functools import wraps
 from api.helpers import Secure
 from flask import request, jsonify
-from api.models import User, Recipe
+from api.models import User, Recipe, RecipeCategory
 
 
 def json_data_required(decorated_func):
@@ -44,6 +44,7 @@ def user_must_own_recipe(decorated_func):
     """
         Ensures that a recipe exists and the user must own a recipe before he changes anything about it
         This decorator must be used below the auth_token_required decorator
+        The decorated function must have two positional arguments in the order user, recipe
     """
 
     @wraps(decorated_func)
@@ -51,7 +52,7 @@ def user_must_own_recipe(decorated_func):
 
         # code to run before decorated function runs
         user = args[0]
-        recipe_id = kwargs.get("recipe_id",None)
+        recipe_id = kwargs.get("recipe_id", None)
 
         recipe = Recipe.query.filter_by(id=recipe_id).first()
 
@@ -66,5 +67,33 @@ def user_must_own_recipe(decorated_func):
 
         # call decorated function
         return decorated_func(user, recipe, *args, **kwargs)
+
+    return wrapper
+
+
+def user_must_own_recipe_category(decorated_func):
+    """
+    Ensures that a recipe category exists and the user must own a recipe category before he changes anything about it
+    This decorator must be used below the auth_token_required decorator
+    :param decorated_func: The function to decorate
+    :return: The wrapper of the decorated function
+    """
+
+    @wraps(decorated_func)
+    def wrapper(*args, **kwargs):
+        user = args[0]
+        recipe_cat_id = kwargs.get("category_id", None)
+
+        recipe_category = RecipeCategory.query.filter_by(id=recipe_cat_id).first()
+
+        if not recipe_category:
+            return jsonify({"errors": ["The recipe category you are trying to modify does not exist"]}), 404
+
+        if recipe_category.owner != user.id:
+            return jsonify({"errors": ["The recipe category you are trying to modify does not belong to you"]}), 403
+
+        kwargs.pop("category_id")
+
+        return decorated_func(user, recipe_category, *args, **kwargs)
 
     return wrapper
