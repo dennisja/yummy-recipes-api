7d72a2f25fb5360c07463c31e01b97c41067e4b9
dennisja <dennisjjagwe@gmail.com>
2017-11-21 14:25:58 +0300

    Enable Cross Origin Resource Sharing to allow Ajax requests from other domains

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import configs
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(configs.get("production"))
 db = SQLAlchemy(app)
+cors = CORS(app)
 
 
 from api import models, routes
